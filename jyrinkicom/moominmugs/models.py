9e242f565233ed87c752b7f9e469236644436e9a
Sanna Jyrinki <sanna.jyrinki@iki.fi>
2022-06-01 01:33:51 +0300

    korjataan ongelmaa

--- jyrinkicom/moominmugs/models.py
+++ jyrinkicom/moominmugs/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Mug(models.Model):
 	nickname = models.CharField(max_length=50)
-	image = models.ImageField(upload_to='images/', blank=True, null=True)
+	image = models.ImageField(upload_to='images/')
 	officialName = models.CharField(max_length=50, blank=True, null=True)
-	figures = models.ManyToManyField('Figure')
-	colors = models.ManyToManyField('Color')
+	figures = models.ManyToManyField('Figure', blank=True, null=True)
+	colors = models.ManyToManyField('Color', blank=True, null=True)
 	theme = models.ForeignKey('Theme', on_delete=models.CASCADE, blank=True, null=True)
 
 	def __str__(self):
-		return self.nickname	
+		return self.nickname
 
 	def get_absolute_url(self):
 		return f"/moominmugs/{self.pk}"
